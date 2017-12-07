e9f3d8885629de6fdfd367eb3acf0156c28e1014
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    clover: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -215,6 +215,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
