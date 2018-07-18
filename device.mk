96eae485c8bd1afd04d134e3d64becdf54cc233d
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    clover: overlay-lineage: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -37,7 +37,8 @@ TARGET_SCREEN_WIDTH := 1200
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
