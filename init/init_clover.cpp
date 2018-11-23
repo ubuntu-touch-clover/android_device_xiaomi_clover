df79e92e1d84104fa8390003c91ffc7123fe6a04
GuaiYiHu <guaiyihu@foxmail.com>
2018-11-23 13:55:45 +0800

    clover: libinit: Set product model dynamically
    Change-Id: I6ac3931cab7301551098d077a7bb60d3760d8d98
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- init/init_clover.cpp
+++ init/init_clover.cpp
@@ -39,6 +39,8 @@
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -47,6 +49,24 @@ using android::base::GetProperty;
 using android::init::property_set;
 using android::base::ReadFileToString;
 using android::base::Trim;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[],
+        char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
 
 static void init_alarm_boot_properties()
 {
@@ -84,5 +104,16 @@ static void init_alarm_boot_properties()
 
 void vendor_load_properties()
 {
+    std::string hw_device;
+
+    char const *hw_id_file = "/sys/devices/platform/HardwareInfo/hw_id";
+
+    ReadFileToString(hw_id_file, &hw_device);
+    if (hw_device.find("D9P") != std::string::npos) {
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4 PLUS");
+    } else {
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4");
+    }
+
     init_alarm_boot_properties();
 }
