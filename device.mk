9cc85aeb9bd8a33cbcb19011939706e49864a456
TH779 <tongsj2333@gmail.com>
2019-07-16 16:13:55 +0800

    clover: Update power HAL to version 1.1
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -305,8 +305,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.sdm660 \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
