GuaiYiHu/android_device_xiaomi_clover-oss

clover: rootdir: Add 'vendor.' prefix to a vendor HAL service name
* To prevent property name collisions between properties of system and
  vendor, 'vendor.' prefix must be added to a vendor HAL service name.
  You can see the details in go/treble-sysprop-compatibility documents.

Test: succeeded building and tested on a sailfish device.
Bug: 36796459
Change-Id: I48e6ee0ae3fe401e39efb273256d991fac676357

rootdir/bin/init.class_main.sh
@@ -50,7 +50,7 @@ esac
 
 case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3" | "sdm" | "sdx")
-    start ipacm
+    start vendor.ipacm
     case "$baseband" in
         "svlte2a" | "csfb")
           start qmiproxy
@@ -69,21 +69,21 @@ case "$baseband" in
     multisim=`getprop persist.radio.multisim.config`
 
     if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-        start ril-daemon2
+        start vendor.ril-daemon2
     elif [ "$multisim" = "tsts" ]; then
-        start ril-daemon2
+        start vendor.ril-daemon2
     fi
 
     case "$datamode" in
         "tethered")
-            start qti
+            start vendor.qti
             ;;
         "concurrent")
-            start qti
-            start netmgrd
+            start vendor.qti
+            start vendor.netmgrd
             ;;
         *)
-            start netmgrd
+            start vendor.netmgrd
             ;;
     esac
 esac

ubuntu-touch-clover/android_device_xiaomi_clover

Signed-off-by: TH779 <tongsj2333@gmail.com>

rootdir/bin/init.class_main.sh
@@ -40,18 +40,18 @@ case "$baseband" in
 #    setprop ro.radio.noril yes
     setprop ro.radio.noril true
 #modify by gongpeicai for wifiOnly bug-end
-    stop ril-daemon
+    stop vendor.ril-daemon
 esac
 
 case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3")
-    start qmuxd
+    start vendor.qmuxd
 esac
 
 case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3" | "sdm" | "sdx")
-    start ipacm-diag
-    start ipacm
+    start vendor.ipacm-diag
+    start vendor.ipacm
     case "$baseband" in
         "svlte2a" | "csfb")
           start qmiproxy
@@ -70,24 +70,24 @@ case "$baseband" in
     multisim=`getprop persist.radio.multisim.config`
 
     if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-        start ril-daemon2
+        start vendor.ril-daemon2
     elif [ "$multisim" = "tsts" ]; then
-        start ril-daemon2
-        start ril-daemon3
+        start vendor.ril-daemon2
+        start vendor.ril-daemon3
     fi
 
     case "$datamode" in
         "tethered")
-            start dataqti
-            start port-bridge
+            start vendor.dataqti
+            start vendor.port-bridge
             ;;
         "concurrent")
-            start dataqti
-            start netmgrd
-            start port-bridge
+            start vendor.dataqti
+            start vendor.netmgrd
+            start vendor.port-bridge
             ;;
         *)
-            start netmgrd
+            start vendor.netmgrd
             ;;
     esac
 esac