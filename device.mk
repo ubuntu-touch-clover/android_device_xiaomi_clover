076518ec5215f65a09b36be9c63792d649042555
HandsomeYingyan <handsomeyingyan@github.com>
2021-10-12 21:16:54 +0800

    Support dynamic CameraProvider HAL

--- device.mk
+++ device.mk
@@ -140,11 +140,19 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    libbson \
+    libgui_vendor \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     libxml2 \
     android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service:32 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
+#    Snap
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
